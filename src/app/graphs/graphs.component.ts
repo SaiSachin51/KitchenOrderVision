bf2c65944d73d7963a4934213f6ce195503bb057
SaiSachin51 <saikumar.kadaveru51@gmail.com>
2023-07-21 20:50:08 +0530

    Asset Utilization

--- src/app/graphs/graphs.component.ts
+++ src/app/graphs/graphs.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./graphs.component.css']
 })
 export class GraphsComponent {
-
+orders: any[] = [
+  {
+    orderTime: '09:00 AM',
+    customerName: 'John Doe',
+    orderDetails: 'Burger, Fries',
+    predictedQuantity: 2
+  },
+  // Add more order data for different days and times
+];
 }
